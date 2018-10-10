a4796689f32876293d644748347d40354feea3ff
Dinh Vu Nam <vunam0502@gmail.com>
2018-10-10 18:01:29 +0700

    add Double+Ext

---
+++ ExtensionProject/Utils/Extensions/Standards/Double/Double+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Double+Ext.swift
+//  ExtensionProject
+//
+//  Created by Dinh Vu Nam on 10/10/18.
+//  Copyright © 2018 Dinh Vu Nam. All rights reserved.
+//
+
+import UIKit
+
+class Double_Ext: NSObject {
+
+}
